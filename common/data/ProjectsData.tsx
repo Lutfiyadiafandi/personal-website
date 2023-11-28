275f50e06cd71b69674a61a81ad107cfce464e2f
Lutfiyadi Afandi <lutfiyadiafandi@gmail.com>
2023-11-28 15:13:27 +0800

    add stack projects

--- common/data/ProjectsData.tsx
+++ common/data/ProjectsData.tsx
@@ -7,27 +7,26 @@ import {
 } from "react-icons/si";
 import { IProjects } from "../interface/IProjects";
 import { FaCss3Alt, FaJs } from "react-icons/fa";
+import { TbBrandNextjs } from "react-icons/tb";
 
 const size = 25;
 export const ProjectsData: IProjects[] = [
   {
     title: "Lutfiyadi Afandi Personal Website",
     image: "/projects/lutfiyadiafandi.png",
-    link: "/projects",
+    link: "https://lutfiyadiafandi.site",
     tech: (
       <>
-        <SiReact size={size} className="text-sky-500" />
+        <TbBrandNextjs size={size} className="text-black dark:text-white" />
         <SiTypescript size={size} className="text-blue-400" />
         <SiTailwindcss size={size} className="text-cyan-300" />
-        <SiStrapi size={size} className="text-blue-500" />
-        <SiNestjs size={size} className="text-rose-500" />
       </>
     ),
   },
   {
     title: "Lentera Visi Edutama",
     image: "/projects/lenteravisiedutama.png",
-    link: "https://lenteravisiedutama.com/",
+    link: "https://lenteravisiedutama.com",
     tech: (
       <>
         <SiReact size={size} className="text-sky-500" />
@@ -41,7 +40,7 @@ export const ProjectsData: IProjects[] = [
   {
     title: "Staycation.",
     image: "/projects/staycation.png",
-    link: "https://la-staycation.netlify.app/",
+    link: "https://la-staycation.netlify.app",
     tech: (
       <>
         <SiReact size={size} className="text-sky-500" />
@@ -53,7 +52,7 @@ export const ProjectsData: IProjects[] = [
   {
     title: "Portofolio V1",
     image: "/projects/portofoliov1.png",
-    link: "https://lutfiyadiafandi.netlify.app/",
+    link: "https://lutfiyadiafandi.netlify.app",
     tech: (
       <>
         <SiReact size={size} className="text-sky-500" />
@@ -65,7 +64,7 @@ export const ProjectsData: IProjects[] = [
   {
     title: "Todo List App",
     image: "/projects/todolist.png",
-    link: "https://apptodo-list.netlify.app/",
+    link: "https://apptodo-list.netlify.app",
     tech: (
       <>
         <SiReact size={size} className="text-sky-500" />
